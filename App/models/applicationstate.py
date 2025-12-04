8c20151a501d3b1db3922250e002c26b18b13fd9
VinayakNM <vinayakmaharaj30@gmail.com>
2025-12-03 20:37:16 -0400

    combined the states in models

--- App/models/applicationstate.py
+++ App/models/applicationstate.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class ApplicationState(ABC):
 
-    def __init__(self, state, description=None):
+    def _init_(self, state, description=None):
         self.state = state
         self.description = description
 
@@ -20,3 +20,59 @@ class ApplicationState(ABC):
     @abstractmethod
     def employer_reject(self, application):
         pass
+
+class ShortlistedState(ApplicationState):
+
+    def _init_(self, description=None):
+        super()._init_('Shortlisted', description)
+
+    def shortlist(self, application):
+        raise Exception("Application is already shortlisted for a position.")
+    
+    def employer_accept(self, application):
+        application.state = "Accepted"
+    
+    def employer_reject(self, application):
+        application.state = "Rejected"
+
+class AppliedState(ApplicationState):
+
+    def _init_(self, description=None):
+        super()._init_('Applied', description)
+
+    def shortlist(self, application):
+        application.state = "Shortlisted"
+    
+    def employer_accept(self, application):
+        raise Exception("Applicant must be shortlisted before acceptance.")
+    
+    def employer_reject(self, application):
+        raise Exception("Applicant must be shortlisted before rejection.")
+
+class AcceptedState(ApplicationState):
+
+    def _init_(self, description=None):
+        super()._init_('Accepted', description)
+
+    def shortlist(self, application):
+        raise Exception("Applicant has already been accepted for a position.")
+    
+    def employer_accept(self, application):
+        raise Exception("Applicant has already been accepted for a position.")
+    
+    def employer_reject(self, application):
+        raise Exception("Cannot reject an applicant who has already been accepted.")
+
+class RejectedState(ApplicationState):
+
+    def _init_(self, description=None):
+        super()._init_('Rejected', description)
+
+    def shortlist(self, application):
+        raise Exception("Applicant has already been rejected for this position.")
+    
+    def employer_accept(self, application):
+        raise Exception("Applicant has already been rejected for this position.")
+    
+    def employer_reject(self, application):
+        raise Exception("Applicant has already been rejected for this position.")
