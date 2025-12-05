cc69ab8d921e53e439c51468433c8e0df7c9dd44
Vinayak Maharaj <123012519+VinayakNM@users.noreply.github.com>
2025-12-04 20:42:36 -0400

    Improve addToShortlist with logging and application state
    Enhanced the addToShortlist function with detailed logging and added application state update.

--- App/controllers/staff.py
+++ App/controllers/staff.py
@@ -3,7 +3,9 @@ from App.models.shortlist import Shortlist
 # from App.models.student import Shortlist
 from App.models.position import Position
 from App.models.student import Student
+from App.models.application import Application
 from App.database import db
+
 
 def create_staff(username, password, employerID):
     sta = Staff(username, password, employerID)
@@ -24,15 +26,52 @@ def get_all_staff():
     return staff
 
 def addToShortlist(staffID, positionID, studentID):
-        staff = Staff.query.filter_by(id=staffID).first()
-        if not staff:
-            return False
-        position = Position.query.filter_by(id=positionID).first()
-        student = Student.query.filter_by(id=studentID).first()
 
-        if position != None and student != None:
-            position.shortlist.append(student)
-            db.session.commit()
-            return True
+    # Find staff member
 
+    staff = Staff.query.filter_by(id=staffID).first()
+
+    # If not found return
+
+    if not staff:
+        print("Staff not found")
         return False
+    print("Staff found")
+    # Find position 
+    
+    position = Position.query.filter_by(id=positionID).first()
+
+    if not position:
+        print("Position not found")
+        return False
+    print("Position found")
+
+    student = Student.query.filter_by(id=studentID).first()
+
+    if not student:
+        print("Student not found")
+        return False
+    print("Student found. Appending to shortlist...")
+
+    # if position != None and student != None:
+
+    # Add the student to the position's shortlist
+
+    position.shortlist.append(student)
+    print("Finding application...")
+    # Get the application from the student and update the state
+
+    application = Application.query.filter_by(student_id=studentID, position_id=positionID).first()
+
+    if not application:
+        print("Application not found")
+        return False
+    
+    print("Setting application state...")
+    
+    application.set_state('Shortlisted')
+    
+    db.session.commit()
+
+    print("Student added to shortlist successfully! Application state: " + application.application_state)
+    return True
